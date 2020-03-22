ada9bcd3509fcbe036b87ace277c4535707f42ba
Adam G Horn <hornadam8@gmail.com>
2020-03-22 14:59:07 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolate','5','M','Brown','friendly','1');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Rowdy','10','M','Brown','aggresive','1');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Grinch','2','M','Brown','mean','1');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Tabitha','6','F','Black','excitable','1');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Sergeant Brown','17','M','Brown','calm','0');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Melissa','13','F','Brown','calm','1');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Wendy','6','F','Black','friendly','1');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES (NULL,'20','M','black','aggresive','0');
