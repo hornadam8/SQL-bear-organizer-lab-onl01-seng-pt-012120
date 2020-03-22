12ddf7bf024502756ee3fe1f67e0560fb4b28fae
Adam G Horn <hornadam8@gmail.com>
2020-03-22 14:26:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Tabitha','4'
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Sergeant Brown','8','M','Brown','calm','false');
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Melissa','7','F','Brown','calm','true');
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Wendy','3','F','Black','friendly','true');
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('','5','M','Brown','savage','false');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES (NULL,'5','M','Brown','savage','false');
