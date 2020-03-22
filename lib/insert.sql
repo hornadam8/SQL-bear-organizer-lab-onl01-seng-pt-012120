8437fbede84d92eaedfe178bec93422aec516369
Adam G Horn <hornadam8@gmail.com>
2020-03-22 14:47:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolate','5','M','Brown','friendly','true');
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Rowdy','10','M','Brown','aggresive','true');
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Grinch','2','M','Brown','mean','true');
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Tabitha','6','F','Black','excitable','true');
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Sergeant Brown','17','M','Brown','calm','false');
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Melissa','13','F','Brown','calm','true');
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Wendy','6','F','Black','friendly','true');
-INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES (NULL,'5','M','Brown','savage','false');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolate','5','M','Brown','friendly','1');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Rowdy','10','M','Brown','aggresive','1');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Grinch','2','M','Brown','mean','1');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Tabitha','6','F','Black','excitable','1');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Sergeant Brown','17','M','Brown','calm','0');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Melissa','13','F','Brown','calm','1');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Wendy','6','F','Black','friendly','1');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES (NULL,'5','M','Brown','savage','0');
