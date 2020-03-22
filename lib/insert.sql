0d7b59c5dd12a16eba8e63d9acfadc55ec472c66
Adam G Horn <hornadam8@gmail.com>
2020-03-22 14:22:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,3 +3,4 @@ INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolat
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolate','5','M','Brown','friendly','true');
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolate','5','M','Brown','friendly','true');
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolate','5','M','Brown','friendly','true');
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolate','5','M','Brown','friendly','true');
